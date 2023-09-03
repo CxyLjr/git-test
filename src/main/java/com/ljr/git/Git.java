e94f4ca4a4a7c4ded082564b13f86667bbe683a8
jingru <2595285668@qq.com>
2023-09-03 13:06:24 +0800

    first commit

---
+++ src/main/java/com/ljr/git/Git.java
@@ -0,0 +1,13 @@
+package com.ljr.git;
+
+/**
+ * @version 1.0
+ * @Author 作者名
+ * @Date 2023/9/2 10:59
+ * @注释
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
