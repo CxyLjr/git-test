6179cc7d326ea98d933d7d3123e2a87bd95d7bdb
jingru <2595285668@qq.com>
2023-09-03 13:20:47 +0800

    hot-fix second commit

--- src/main/java/com/ljr/git/Git.java
+++ src/main/java/com/ljr/git/Git.java
@@ -11,5 +11,6 @@ public class Git {
         System.out.println("hello,git");
         System.out.println("hello,git2");
         System.out.println("hello,git3 hot-fix");
+        System.out.println("hello,hot-fix test");
     }
 }
